0618515b975b26cae4ffda3acf4dc780086ef121
David Cristofaro <david@dtcristo.com>
2011-12-20 17:50:38 +1100

    Finish change of License

--- src/com/dtcristo/virtucane/ImageProcessor.java
+++ src/com/dtcristo/virtucane/ImageProcessor.java
@@ -1,24 +1,18 @@
-/* 
- * Copyright (C) 2011  David Cristofaro
+/*
+ * Copyright 2011 David Cristofaro
+ * Copyright 2011 Monash University
  * 
- * Email:      d@davidcristofaro.com
- * Homepage:   http://davidcristofaro.com
- * Repository: https://github.com/dtcristo/virtucane
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * This file is part of virtucane.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Virtucane is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Virtucane is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with virtucane.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.dtcristo.virtucane;
