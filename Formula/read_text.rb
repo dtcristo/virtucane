516446e06024aeae9f04b250ea9872f811b58911
David Cristofaro <david@dtcristo.com>
2012-02-17 10:44:41 +1100

    Add Homebrew formula

---
+++ Formula/read_text.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class ReadText < Formula
+  head 'https://github.com/dtcristo/read_text.git'
+  homepage 'https://github.com/dtcristo/read_text'
+
+  depends_on 'tesseract'
+  depends_on 'opencv'
+  depends_on 'boost'
+
+  def install
+    system "make"
+    bin.install "read_text"
+  end
+end
