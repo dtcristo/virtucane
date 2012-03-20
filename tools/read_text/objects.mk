6ef598a3fc1b1e5129050b5af2b55a9bfc4b41ec
David Cristofaro <git@dtcristo.com>
2012-03-20 18:18:51 +1100

    Remove read_text makefiles

--- tools/read_text/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_program_options-mt -lopencv_core -lopencv_highgui -lopencv_video -lopencv_imgproc
-
