8f12b16def4a8b3e16e49048f6296ee329fe7523
David Cristofaro <david@dtcristo.com>
2012-03-20 18:11:26 +1100

    New tool: pp_detect

---
+++ tools/pp_detect/src/pp_detect.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : pp_detect.cpp
+// Author      : David Cristofaro
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
