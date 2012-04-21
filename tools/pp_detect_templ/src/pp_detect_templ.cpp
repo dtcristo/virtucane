cf0fa943f833ce5bdcdfa9d72af8a20a8b28ea04
David Cristofaro <git@dtcristo.com>
2012-04-21 12:01:06 +1000

    Rename variables

--- tools/pp_detect_templ/src/pp_detect_templ.cpp
+++ tools/pp_detect_templ/src/pp_detect_templ.cpp
@@ -11,7 +11,7 @@ using namespace std;
 using namespace cv;
 
 /// Global Variables
-Mat binary;
+Mat scene;
 Mat templ;
 Mat result;
 string image_window = "Source Image";
@@ -28,7 +28,7 @@ void MatchingMethod(int, void*);
  */
 int main(int argc, char** argv) {
 
-	Mat frame, gray;
+	Mat frame, gray, binary;
 
 	VideoCapture capture(0);
 	if (!capture.isOpened())
@@ -50,11 +50,11 @@ int main(int argc, char** argv) {
 		capture.grab();
 		capture.retrieve(frame);
 
-		cvtColor(frame, gray, CV_RGB2GRAY);
+		//cvtColor(frame, gray, CV_RGB2GRAY);
 		//threshold(gray, binary, 10, 255, THRESH_OTSU);
 		//cvtColor(binary, binary, CV_GRAY2RGB);
 
-		binary = gray;
+		scene = frame;
 
 		//imshow(window_name, binary);
 
@@ -74,16 +74,16 @@ int main(int argc, char** argv) {
 void MatchingMethod(int, void*) {
 	/// Source image to display
 	Mat img_display;
-	binary.copyTo(img_display);
+	scene.copyTo(img_display);
 
 	/// Create the result matrix
-	int result_cols = binary.cols - templ.cols + 1;
-	int result_rows = binary.rows - templ.rows + 1;
+	int result_cols = scene.cols - templ.cols + 1;
+	int result_rows = scene.rows - templ.rows + 1;
 
 	result.create(result_cols, result_rows, CV_32FC1);
 
 	/// Do the Matching and Normalize
-	matchTemplate(binary, templ, result, match_method);
+	matchTemplate(scene, templ, result, match_method);
 	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
 	/// Localizing the best match with minMaxLoc
