f150998883a8b7eaef6ff6022045c8c5fc123c8e
David Cristofaro <david@dtcristo.com>
2012-04-22 15:00:17 +1000

    Add multiple plug searching

--- tools/pp_detect_templ/src/pp_detect_templ.cpp
+++ tools/pp_detect_templ/src/pp_detect_templ.cpp
@@ -1,36 +1,28 @@
-/**
- * @file MatchTemplate_Demo.cpp
- * @brief Sample code to use the function MatchTemplate
- * @author OpenCV team
- */
-
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
-/// Global Variables
-Mat scene;
-Mat templ;
-Mat result;
-string image_window = "Source Image";
-string result_window = "Result window";
+string frame_window = "Webcam";
+string image_window = "Detected";
+string result_window = "Result";
 
-int match_method;
-int max_Trackbar = 5;
+vector<Point> getMatches(Mat I, float thresh, bool max);
 
-/// Function Headers
-void MatchingMethod();
-vector<Point> getMatches(Mat I, int thresh, bool max);
-
-/**
- * @function main
- */
 int main(int argc, char** argv) {
 
-	int threshold = 100;
-	Mat frame, gray, binary;
+	int match_method = 5;
+	float threshold = 0.5;
+
+	Mat templ;
+	Mat frame;
+	Mat gray;
+	Mat binary;
+	Mat scene;
+	Mat result;
+	Mat result_norm;
+	Mat img_display;
 
 	VideoCapture capture(0);
 	if (!capture.isOpened())
@@ -38,129 +30,108 @@ int main(int argc, char** argv) {
 
 	templ = imread("templ.jpg", 1);
 
+	namedWindow(frame_window, CV_WINDOW_AUTOSIZE);
 	namedWindow(image_window, CV_WINDOW_AUTOSIZE);
 	namedWindow(result_window, CV_WINDOW_AUTOSIZE);
 
-	/// Create Trackbar
-	string trackbar_label =
-			"Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-	createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar,
-			NULL);
-
 	for (;;) {
-
 		capture.grab();
 		capture.retrieve(frame);
+		imshow(frame_window, frame);
 
-		cvtColor(frame, gray, CV_RGB2GRAY);
+		//cvtColor(frame, gray, CV_RGB2GRAY);
 		//threshold(gray, binary, 10, 255, THRESH_OTSU);
 		//cvtColor(binary, binary, CV_GRAY2RGB);
 
 		scene = frame;
 
-		//imshow(image_window, gray);
-
-		//MatchingMethod();
-
 		int c = waitKey(30);
 		if ((char) c == 'q') {
 			break;
 		} else if ((char) c == 'x') {
-			threshold += 10;
-			cout << "Threshold +10 = " << threshold << endl;
+			threshold += 0.01;
+			cout << "Threshold +0.01 = " << threshold << endl;
 		} else if ((char) c == 'z') {
-			threshold -= 10;
-			cout << "Threshold -10 = " << threshold << endl;
-		} else if ((char) c == 'p') {
+			threshold -= 0.01;
+			cout << "Threshold -0.01 = " << threshold << endl;
+		} else if ((char) c == '1') {
+			match_method = 1;
+			cout << "match_method = SQDIFF (" << match_method << ")" << endl;
+		} else if ((char) c == '2') {
+			match_method = 2;
+			cout << "match_method = SQDIFF NORMED (" << match_method << ")"
+					<< endl;
+		} else if ((char) c == '3') {
+			match_method = 3;
+			cout << "match_method = TM CCORR (" << match_method << ")" << endl;
+		} else if ((char) c == '4') {
+			match_method = 4;
+			cout << "match_method = TM COEFF (" << match_method << ")" << endl;
+		} else if ((char) c == '5') {
+			match_method = 5;
+			cout << "match_method = TM COEFF NORMED (" << match_method << ")"
+					<< endl;
+		} else if ((char) c == 'r') {
 
-			cout << "\nPrinting points" << endl;
-			vector<Point> matchedPoints = getMatches(gray, threshold, false);
+			scene.copyTo(img_display);
 
-			for (int i = 0; i < matchedPoints.size(); i++) {
-				cout << "x = " << matchedPoints[i].x << "\ty = "
-						<< matchedPoints[i].y << endl;
-				circle(frame, matchedPoints[i], 5, Scalar(0, 0, 255));
+			int result_cols = scene.cols - templ.cols + 1;
+			int result_rows = scene.rows - templ.rows + 1;
+			result.create(result_cols, result_rows, CV_32FC1);
 
+			matchTemplate(scene, templ, result, match_method);
+			normalize(result, result_norm, 0, 1, NORM_MINMAX, -1, Mat());
+
+			double minVal;
+			double maxVal;
+			Point minLoc;
+			Point maxLoc;
+			Point matchLoc;
+
+			vector<Point> matched;
+
+			minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+			//matchedPoints.push_back(maxLoc);
+			cout << "max = " << result.at<float>(maxLoc) << endl;
+
+			if (match_method == CV_TM_SQDIFF
+					|| match_method == CV_TM_SQDIFF_NORMED) {
+				matched = getMatches(result, threshold, false);
+			} else {
+				matched = getMatches(result, threshold, true);
 			}
-			imshow(image_window, frame);
+
+			for (vector<Point>::iterator it = matched.begin();
+					it != matched.end(); ++it) {
+				rectangle(img_display, *it,
+						Point(it->x + templ.cols, it->y + templ.rows),
+						Scalar(0, 0, 255));
+				rectangle(result_norm, *it,
+						Point(it->x + templ.cols, it->y + templ.rows),
+						Scalar(0, 0, 255));
+			}
+
+			imshow(image_window, img_display);
+			imshow(result_window, result_norm);
 		}
 	}
-
 	return 0;
 }
 
-/**
- * @function MatchingMethod
- * @brief Trackbar callback
- */
-void MatchingMethod() {
-	/// Source image to display
-	Mat img_display;
-	scene.copyTo(img_display);
-
-	/// Create the result matrix
-	int result_cols = scene.cols - templ.cols + 1;
-	int result_rows = scene.rows - templ.rows + 1;
-
-	result.create(result_cols, result_rows, CV_32FC1);
-
-	/// Do the Matching and Normalize
-	matchTemplate(scene, templ, result, match_method);
-	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
-
-	/// Localizing the best match with minMaxLoc
-	double minVal;
-	double maxVal;
-	Point minLoc;
-	Point maxLoc;
-	Point matchLoc;
-
-	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-
-	/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-	if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED) {
-		matchLoc = minLoc;
-	} else {
-		matchLoc = maxLoc;
-	}
-
-	/// Show me what you got
-	rectangle(img_display, matchLoc,
-			Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows),
-			Scalar::all(0), 2, 8, 0);
-	rectangle(result, matchLoc,
-			Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows),
-			Scalar::all(0), 2, 8, 0);
-
-	imshow(image_window, img_display);
-	imshow(result_window, result);
-
-	return;
-}
-
-vector<Point> getMatches(Mat I, int thresh, bool max = true) {
-
-	// accept only char type matrices
-	CV_Assert(I.depth() != sizeof(uchar));
+vector<Point> getMatches(Mat I, float thresh, bool max) {
 
 	assert(I.channels() == 1);
 
 	vector<Point> matchPoints;
 
-	int nRows = I.rows;
-	int nCols = I.cols;
-
-	int i, j;
-	uchar* p;
-	for (i = 0; i < nRows; ++i) {
-		p = I.ptr<uchar>(i);
-		for (j = 0; j < nCols; ++j) {
+	for (int i = 0; i < I.rows; i++) {
+		for (int j = 0; j < I.cols; j++) {
 			if (max) {
-				if (p[j] >= thresh) {
+				if (I.at<float>(i, j) >= thresh) {
 					matchPoints.push_back(Point(j, i));
 				}
 			} else {
-				if (p[j] <= thresh) {
+				if (I.at<float>(i, j) <= thresh) {
 					matchPoints.push_back(Point(j, i));
 				}
 			}
