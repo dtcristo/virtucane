364d03618abf456a1d8a3a08b834846653031698
David Cristofaro <git@dtcristo.com>
2012-05-11 20:11:49 +1000

    Release mode with optimisations

--- tools/pp_detect_templ/src/pp_detect_templ.cpp
+++ tools/pp_detect_templ/src/pp_detect_templ.cpp
@@ -1,5 +1,8 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <ctime>
+#include <unistd.h>
+#include <sys/time.h>
 
 using namespace std;
 using namespace cv;
@@ -13,8 +16,8 @@ int
 main(int argc, char** argv)
 {
     int matchMethod = 5;
-    float threshold = 0.4;
-    int numTempl = 6;
+    float threshold = 0.6;
+    int numTempl = 7;
     float scaleFactor = 0.7;
 
     Mat templ;
@@ -23,27 +26,39 @@ main(int argc, char** argv)
 
     VideoCapture capture(0);
     if (!capture.isOpened())
+    {
+        cout << "Could not open VideoCapture." << endl;
         return -1;
+    }
 
     string frameWindow = "Webcam";
     string matchWindow = "Matches";
     namedWindow(frameWindow, CV_WINDOW_AUTOSIZE);
     namedWindow(matchWindow, CV_WINDOW_AUTOSIZE);
 
-    cvtColor(imread("templ.png", 1), templ, CV_RGB2GRAY);
+    cvtColor(imread("crop100.png", 1), templ, CV_RGB2GRAY);
 
     vector<Mat> templates;
     vector<Size> sizes;
     scaleTempl(templ, templates, sizes, scaleFactor, numTempl);
 
+    int framecount = 0;
+    timeval start;
+    gettimeofday(&start, 0);
     for (;;)
     {
         capture >> frame;
         imshow(frameWindow, frame);
         cvtColor(frame, gray, CV_RGB2GRAY);
 
-        vector<Rect> matches;
+        //vector<Rect> matches;
+        float largest_val = 0;
+        Point largest_point;
+        Size largest_size;
 
+        //timeval a;
+        //timeval b;
+        //gettimeofday(&a, 0);
         for (vector<Mat>::iterator it = templates.begin(); it != templates.end(); ++it)
         {
             Mat result;
@@ -59,28 +74,44 @@ main(int argc, char** argv)
 //            normalize(result, resultNorm, 0, 1, NORM_MINMAX, -1, Mat());
 //            cvtColor(resultNorm, resultNorm, CV_GRAY2RGB);
 
-//            double minVal;
-//            double maxVal;
-//            Point minLoc;
-//            Point maxLoc;
+            double minVal;
+            double maxVal;
+            Point minLoc;
+            Point maxLoc;
 
-//            minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-//            matchedPoints.push_back(maxLoc);
+            minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
 
-            getMatches(result, s, matches, threshold);
+            float current = result.at<float>(maxLoc);
+            if ((current >= threshold) && (current > largest_val))
+            {
+                largest_val = current;
+                largest_point = maxLoc;
+                largest_size = s;
+            }
+
+//            if (matches.size() < 5)
+//                getMatches(result, s, matches, threshold);
         }
+        //gettimeofday(&b, 0);
+        //cout << "Time in msec = " << (b.tv_usec - a.tv_usec)/1000 << endl;
 
         cvtColor(gray, gray, CV_GRAY2RGB);
 
-        for (vector<Rect>::iterator it = matches.begin(); it != matches.end(); ++it)
-            rectangle(gray, *it, Scalar(0, 0, 255));
+        rectangle(gray, Rect(largest_point, largest_size), Scalar(255, 0, 0));
+
+//        for (vector<Rect>::iterator it = matches.begin(); it != matches.end(); ++it)
+//            rectangle(gray, *it, Scalar(255, 0, 0));
 
         imshow(matchWindow, gray);
 
-        // Wait 30ms and poll keystrokes.
-        int c = waitKey(30);
+        int c = waitKey(1);
         if ((char) c == 27 /*ESC key*/)
         {
+            timeval end;
+            gettimeofday(&end, 0);
+            int seconds = (end.tv_sec - start.tv_sec);
+            int fps = framecount/seconds + 0.5;
+            cout << "fps = " << fps << endl;
             break;
         }
         else if ((char) c == 'x')
@@ -118,6 +149,7 @@ main(int argc, char** argv)
             matchMethod = 5;
             cout << "matchMethod = TM COEFF NORMED (5)" << endl;
         }
+        framecount++;
     }
 
     return 0;
